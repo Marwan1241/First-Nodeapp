7f096d9fc2b2bd16dae35925e528f5e66c40131c
marwan1241 <marwanmagdi.2001@gmail.com>
2021-05-08 12:15:26 +0200

    Node app

---
+++ delegates/public/js/script.js
@@ -0,0 +1,168 @@
+var sundayTime = ['4:00','4:20','4:40','5:00','5:20','5:40','6:00','8:00','8:20','8:40','9:00','9:20','9:40','10:00','10:20','10:40','11:00'];
+var mondayTime = ['4:00','4:20','4:40','5:00','5:20','5:40','6:00','8:00','8:20','8:40','9:00','9:20','9:40','10:00','10:20','10:40','11:00'];
+var tuesdayTime = ['4:00','4:20','4:40','5:00','5:20','5:40','6:00','8:00','8:20','8:40','9:00','9:20','9:40','10:00','10:20','10:40','11:00'];
+var wednesdayTime = ['4:00','4:20','4:40','5:00','5:20','5:40','6:00','8:00','8:20','8:40','9:00','9:20','9:40','10:00','10:20','10:40','11:00'];
+var thursdayTime = ['4:00','4:20','4:40','5:00','5:20','5:40','6:00','8:00','8:20','8:40','9:00','9:20','9:40','10:00','10:20','10:40','11:00'];
+var fridayTime = ['4:00','4:20','4:40','5:00','5:20','5:40','6:00','8:00','8:20','8:40','9:00','9:20','9:40','10:00','10:20','10:40','11:00'];
+var saturdayTime = ['4:00','4:20','4:40','5:00','5:20','5:40','6:00','8:00','8:20','8:40','9:00','9:20','9:40','10:00','10:20','10:40','11:00'];
+
+var select = document.getElementById('time');
+var timeRem = document.getElementsByTagName('option');
+
+
+
+
+/* $(document).ready(function(){
+    $.get("done", function(data, status){
+        let len = thursdayTime.length;
+        var alreadySelectedArray = [];
+        $.each(data,function(key,val){
+            alreadySelectedArray.push(val.thursdayTime);
+        });
+        
+        for (var i = 0 ; i < len ; i++){
+            var opt = document.createElement('option');
+            opt.value = thursdayTime[i];
+            opt.innerHTML = thursdayTime[i];
+            if(!alreadySelectedArray.includes(thursdayTime[i])){
+                select.appendChild(opt);
+            }
+        }
+      });
+}); */ 
+
+
+ $(document).ready(function () {
+    $("#day").change(function () {  
+        var selectedDay = $(this).val();
+        if (selectedDay == "Thursday 6/7") {
+            $.get(`done?day=${selectedDay}`, function(data, status){
+                let len = thursdayTime.length;
+                var alreadySelectedArray = [];
+                $("#time").html("");
+                console.log(data);
+                $.each(data,function(key,val){
+                    alreadySelectedArray.push(val.time);
+                });
+                for (var i = 0 ; i < len ; i++){
+                    var opt = document.createElement('option');
+                    opt.value = thursdayTime[i];
+                    opt.innerHTML = thursdayTime[i];
+                    if(!alreadySelectedArray.includes(thursdayTime[i])){
+                        select.appendChild(opt);
+                    }
+                }
+            });
+        } else if (selectedDay == "Friday 7/7") {
+            $.get(`done?day=${selectedDay}`, function(data, status){
+                let len = fridayTime.length;
+                var alreadySelectedArray = [];
+                $("#time").html("");
+                $.each(data,function(key,val){
+                    alreadySelectedArray.push(val.time);
+                });
+                
+                for (var i = 0 ; i < len ; i++){
+                    var opt = document.createElement('option');
+                    opt.value = fridayTime[i];
+                    opt.innerHTML = fridayTime[i];
+                    if(!alreadySelectedArray.includes(fridayTime[i])){
+                        select.appendChild(opt);
+                    }
+                }
+              });
+        } else if (selectedDay == "Saturday 8/7") {
+            $.get(`done?day=${selectedDay}`, function(data, status){
+                let len = saturdayTime.length;
+                var alreadySelectedArray = [];
+                $("#time").html("");
+                $.each(data,function(key,val){
+                    alreadySelectedArray.push(val.time);
+                });
+                
+                for (var i = 0 ; i < len ; i++){
+                    var opt = document.createElement('option');
+                    opt.value = saturdayTime[i];
+                    opt.innerHTML = saturdayTime[i];
+                    if(!alreadySelectedArray.includes(saturdayTime[i])){
+                        select.appendChild(opt);
+                    }
+                }
+              });
+        } else if (selectedDay == "Sunday 9/7") {
+            $.get(`done?day=${selectedDay}`, function(data, status){
+                let len = sundayTime.length;
+                var alreadySelectedArray = [];
+                $("#time").html("");
+                $.each(data,function(key,val){
+                    alreadySelectedArray.push(val.time);
+                });
+                
+                for (var i = 0 ; i < len ; i++){
+                    var opt = document.createElement('option');
+                    opt.value = sundayTime[i];
+                    opt.innerHTML = sundayTime[i];
+                    if(!alreadySelectedArray.includes(sundayTime[i])){
+                        select.appendChild(opt);
+                    }
+                }
+              });
+        } else if (selectedDay == "Monday 10/7") {
+            $.get(`done?day=${selectedDay}`, function(data, status){
+                let len = mondayTime.length;
+                var alreadySelectedArray = [];
+                $("#time").html("");
+                $.each(data,function(key,val){
+                    alreadySelectedArray.push(val.time);
+                });
+                
+                for (var i = 0 ; i < len ; i++){
+                    var opt = document.createElement('option');
+                    opt.value = mondayTime[i];
+                    opt.innerHTML = mondayTime[i];
+                    if(!alreadySelectedArray.includes(mondayTime[i])){
+                        select.appendChild(opt);
+                    }
+                }
+              });
+        } else if (selectedDay == "Tuesday 11/7") {
+            $.get(`done?day=${selectedDay}`, function(data, status){
+                let len = tuesdayTime.length;
+                var alreadySelectedArray = [];
+                $("#time").html("");
+                $.each(data,function(key,val){
+                    alreadySelectedArray.push(val.time);
+                });
+                
+                for (var i = 0 ; i < len ; i++){
+                    var opt = document.createElement('option');
+                    opt.value = tuesdayTime[i];
+                    opt.innerHTML = tuesdayTime[i];
+                    if(!alreadySelectedArray.includes(tuesdayTime[i])){
+                        select.appendChild(opt);
+                    }
+                }
+              });
+        } else if (selectedDay == "Wednesday 12/7") {
+            $.get(`done?day=${selectedDay}`, function(data, status){
+                let len = wednesdayTime.length;
+                var alreadySelectedArray = [];
+                $("#time").html("");
+                $.each(data,function(key,val){
+                    alreadySelectedArray.push(val.time);
+                });
+                
+                for (var i = 0 ; i < len ; i++){
+                    var opt = document.createElement('option');
+                    opt.value = wednesdayTime[i];
+                    opt.innerHTML = wednesdayTime[i];
+                    if(!alreadySelectedArray.includes(wednesdayTime[i])){
+                        select.appendChild(opt);
+                    }
+                }
+              });
+        }
+    });
+}); 
+
+
---
+++ delegates/routes/users.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../lib/db');
+router.get('/form', function(req, res, next) { 
+res.render('index'); 
+});
+router.post('/create', function(req, res, next) {
+  
+  // store all the user input data
+  const userDetails=req.body;
+  
+ 
+  // insert user data into users table
+  var sql = 'INSERT INTO users SET ?';
+  db.query(sql, userDetails,function (err, data) { 
+      if (err) throw err;
+         console.log("User dat is inserted successfully "); 
+  });
+
+ res.redirect('/users/form');  // redirect to user form page after inserting the data
+}); 
+
+router.get('/done', function(req, res, next) {
+  var sql ="SELECT `day` , `time` FROM users WHERE `day` LIKE '%"+req.query.day+"%'";
+  db.query(sql,function (err, data) { 
+    res.send(data);
+      if (err) throw err;
+         console.log("User info is available "); 
+  });
+}); 
+module.exports = router;
