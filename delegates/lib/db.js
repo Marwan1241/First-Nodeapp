fa228c40c13ce086e863edbc10f73526f84e98b7
Marwan1241 <53134691+Marwan1241@users.noreply.github.com>
2021-05-08 12:40:14 +0200

    Update db.js
    Updated user and pass

--- delegates/lib/db.js
+++ delegates/lib/db.js
@@ -1,8 +1,8 @@
 var mysql=require('mysql');
  var connection=mysql.createConnection({
    host:'localhost',
-   user:'root',
-   password:'',
+   user:'superuser',
+   password:'AWSRQOAdfh9Mmprf',
    database:'delegates'
  });
 connection.connect(function(error){
